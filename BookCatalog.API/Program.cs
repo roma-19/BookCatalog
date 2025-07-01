36a750e8a7dd15910a3a9a99000673eb36fffc46
roma-19 <122823516+roma-19@users.noreply.github.com>
2025-07-01 14:23:39 +0300

    Add Swagger configuration

--- BookCatalog.API/Program.cs
+++ BookCatalog.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 namespace BookCatalog.API;
 
 public class Program
@@ -6,25 +8,27 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookCatalog API", Version = "v1"});
+        });
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
+        
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookCatalog API v1");
+                c.RoutePrefix = string.Empty;
+            });
         }
 
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
-
         app.MapControllers();
 
         app.Run();
